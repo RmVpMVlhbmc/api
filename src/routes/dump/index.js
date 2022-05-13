08d8c8a69901c378bc59487756c01bcfdf50ea9a
Fei Yang <me@feiyang.moe>
2022-05-13 04:45:05 +0000

    feat(route): Add more properties to dump route.

--- src/routes/dump/index.js
+++ src/routes/dump/index.js
@@ -3,13 +3,20 @@ import { hasBody } from 'milliparsec'
 function dump(req, res) {
   const data = {
     headers: req['headers'],
+    hostname: req['hostname'],
     ip: req['ip'],
     method: req['method'],
-    path: req['path']
+    path: req['path'],
+    protocol: req['protocol'],
+    url: req['originalUrl']
   }
 
   if (hasBody(req['method'])) {
     data['body'] = req['body']
+  }
+
+  if (req['path'] !== req['originalUrl']) {
+    data['query'] = req['query']
   }
 
   res.json(data)
