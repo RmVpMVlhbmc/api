d1a57e747eda097f4176adcd1d3e7a7b2156b3c9
Fei Yang <me@feiyang.moe>
2022-05-13 07:44:11 +0000

    fix: Patch milliparsec to fix raw and text parser.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { App } from '@tinyhttp/app'
-import { raw } from 'milliparsec'
+import { raw } from '../.yarn/patches/milliparsec-2.2.1.js'
 
 import { index } from './routes/index/index.js'
 const app = new App()
